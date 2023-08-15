42369c0ef190dfb5cf02e2b0c608943bbec737e5
your name <sriwljitthitima@gmail.com>
2023-08-15 23:59:28 +0700

    up

---
+++ Homework/HW01/hw03-1-codex.cpp
@@ -0,0 +1,15 @@
+//TODO: #8 Arrange this C coding into CODEX pattern.
+#include <stdio.h>
+#include <conio.h>
+int main()
+ {
+  int i = 10 ;
+for( int i = 0 ; i <= "%d"- 30 ; i++ ) {
+  if( i < 30 ) {
+  printf( "Here" ) ;
+   if( i==8 ) {
+    printf( "This is 8. " ) ;
+    }
+  } printf( "ok" ) ;
+} return 0 ;
+}
